17775337b096ac2a9023760d43004556f179e936
Lưu Quốc Huy <125254383+LQu-Huy@users.noreply.github.com>
2023-11-17 16:09:04 +0700

    Update jdoodle.c

--- jdoodle.c
+++ jdoodle.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 
 int main() {
-    printf(" Hello mấy bẹn\n Mình tên là Huy\n Mình hocj lớp CĐ-CNTT17A");
+    printf(" Hello mấy bẹn\n Mình tên là Huy\n Mình học lớp CĐ-CNTT17A");
 }
